e220931003ad81f80a79a0171470e67f4f597530
aliintaa <atkzn.05@mail.ru>
2025-03-19 15:00:27 +0300

    //

---
+++ FoodDiplomAlina/Models/Article.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.Runtime.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FoodDiplomAlina.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Razor.Runtime package into your project
+    [HtmlTargetElement("tag-name")]
+    public class Article : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
